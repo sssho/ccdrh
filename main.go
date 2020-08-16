4bb53442e1119d944bf0cd86c153852d345f13e9
sssho <43515414+sssho@users.noreply.github.com>
2020-08-16 13:42:39 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+// A command line tool for clean zsh cdr history file
+// This tool removes non exisitng directories from the history file
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func existingDirectory(r io.Reader) io.Reader {
+	var buffer bytes.Buffer
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		dir := strings.Trim(scanner.Text()[1:], "'")
+		fineinfo, err := os.Stat(dir)
+		if err != nil {
+			continue
+		}
+
+		if fineinfo.IsDir() {
+			buffer.WriteString(scanner.Text() + "\n")
+		}
+	}
+
+	return &buffer
+}
+
+func backupCachefile(cacheFile string, backupFile string) error {
+	src, err := os.Open(cacheFile)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(backupFile)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateCachefile(cacheFile string) error {
+	backupFile := cacheFile + ".bkup"
+	err := backupCachefile(cacheFile, backupFile)
+	if err != nil {
+		return err
+	}
+
+	ifile, err := os.Open(backupFile)
+	if err != nil {
+		return err
+	}
+	defer ifile.Close()
+
+	// Overwrite cacheFile
+	ofile, err := os.Create(cacheFile)
+	if err != nil {
+		return err
+	}
+	defer ofile.Close()
+
+	validDirs := existingDirectory(ifile)
+	_, err = io.Copy(ofile, validDirs)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	cacheFile := filepath.Join(os.Getenv("HOME"), ".cache", "shell", "chpwd-recent-dirs")
+
+	err := updateCachefile(cacheFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
